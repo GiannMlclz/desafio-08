6998c3d59f2b1eb2a00898c1a09ce8ccc544fbdb
Gianfranco Malacalza <gianmlclz@gmail.com>
2024-12-08 12:38:46 -0300

    Desafio n°8. JavaScript, realizamos el ejercicio 1 de crear variables

---
+++ README.md
@@ -0,0 +1,27 @@
+# Desafio N° 08: JavaScript
+
+## Desafio n°1
+
+1. Debes crear las variables nombre, apellido, edad, pais y conocimientos, esta última deberá ser un array.
+- Mostrar todos los valores de las variables por la consola. Los elementos del array mostrarlos en forma manual.
+- Después cambia el valor de edad y añade un nuevo element al array.
+- Muestra esos nuevos valores por la consola.
+
+```sh
+Usamos console.log() Para mostrar mensajes en la consola
+Usamos console.warn() Para mostrar mensajes en la consola de color amarillo
+
+Let para crear variables.
+
+let nombre = 'Gianfranco'
+
+let apellido = 'Malacalza'
+
+let edad = 19
+
+console.log(nombre)
+
+console.log(apellido)
+
+console.log(edad)
+```
---
+++ main.js
@@ -0,0 +1,39 @@
+import './style.css'
+
+/* ------------------------------------------- */
+/* ----------JavaScript: Desafio 08----------- */
+/* ------------------------------------------- */
+
+console.warn('/---------------------/')
+console.warn('Desafio N°8: JavaScript')
+console.warn('/---------------------/')
+console.warn('Ejecicio n°1')
+
+let nombre = 'Gianfranco'
+
+let apellido = 'Malacalza'
+
+let edad = 19
+
+let pais = 'Argentina'
+
+let arrayConocimientos = ['HTML', 'CSS']
+
+console.log(nombre)
+
+console.log(apellido)
+
+console.log(edad)
+
+console.log(pais)
+
+console.log(arrayConocimientos)
+
+let conocimientoNuevo = 'JavaScript'
+
+arrayConocimientos.push(conocimientoNuevo)
+
+console.log (arrayConocimientos)
+
+
+
